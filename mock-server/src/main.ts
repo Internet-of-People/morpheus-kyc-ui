d32b0d3cb22e5b366274b0c5df093de9fb146d37
Sandor Muranyi <contact@mudlee.hu>
2020-01-09 09:03:28 +0100

    Using evidence schema and 'valid' JsonSchema

--- mock-server/src/main.ts
+++ mock-server/src/main.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { Application } from 'express';
-import confirmedEUCitizenSchema from './confirmedEUCitizenSchema.json';
+import confirmedEUCitizenClaimSchema from './confirmedEUCitizenClaimSchema.json';
+import confirmedEUCitizenEvidenceSchema from './confirmedEUCitizenEvidenceSchema.json';
 import processes from './processes.json';
 
 class Server {
@@ -14,19 +15,24 @@ class Server {
   public start(): void {
     this.app.get(`${Server.ROOT_PATH}/processes/list`, async (_, res): Promise<void> => {
       res.status(200).json({
-        // http://127.0.0.1:8080/morpheus/witness-service/claim-schemas/confirmedEUCitizen
+        // claimSchema: http://127.0.0.1:8080/morpheus/witness-service/claim-schemas/confirmedEUCitizen
+        // evidenceSchema: http://127.0.0.1:8080/morpheus/witness-service/evidence-schemas/confirmedEUCitizen
         processes,
       });
     });
 
     this.app.get(`${Server.ROOT_PATH}/claim-schemas/confirmedEUCitizen`, async (_, res): Promise<void> => {
-      res.status(200).json(confirmedEUCitizenSchema);
+      res.status(200).json(confirmedEUCitizenClaimSchema);
+    });
+
+    this.app.get(`${Server.ROOT_PATH}/evidence-schemas/confirmedEUCitizen`, async (_, res): Promise<void> => {
+      res.status(200).json(confirmedEUCitizenEvidenceSchema);
     });
 
     this.app.post(`${Server.ROOT_PATH}/witness-request/confirmedEUCitizen/sign`, async (req, res): Promise<void> => {
       const { body } = req;
       console.log(body); // signed claim request
-      res.status(200).json({"soon":"it will here"});
+      res.status(200).json({"soon":"it will be here"});
     });
 
     this.app.listen(8080, (): void => {
