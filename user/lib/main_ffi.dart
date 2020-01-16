4b42d200dfb977c82c7d477d89cde42e6171d99d
Sandor Muranyi <contact@mudlee.hu>
2020-01-16 18:24:40 +0100

    testing Rust-Dart interop callbacks

--- user/lib/main_ffi.dart
+++ user/lib/main_ffi.dart
@@ -2,15 +2,33 @@ import 'dart:ffi';
 
 import 'package:ffi/ffi.dart';
 
-typedef NativeRustPingFunction = Pointer<Utf8> Function(Pointer<Utf8>);
-typedef NativePingFunction = Pointer<Utf8> Function(Pointer<Utf8>);
+typedef rust_ping = Pointer<Utf8> Function(Pointer<Utf8>);
+typedef Ping = Pointer<Utf8> Function(Pointer<Utf8>);
+
+typedef rust_ping_async = Void Function(Pointer<Utf8>, Pointer);
+typedef PingAsync = void Function(Pointer<Utf8>, Pointer);
+
+typedef Callback = Void Function(Pointer<Utf8>);
+
+void callback(Pointer<Utf8> result){
+  print(Utf8.fromUtf8(result));
+}
 
 void main(){
   const path = 'rust_c/target/release/libnative_add.so';
-  DynamicLibrary dl = DynamicLibrary.open(path);
-  final ping = dl
-      .lookup<NativeFunction<NativeRustPingFunction>>('ping')
-      .asFunction<NativePingFunction>();
+  DynamicLibrary lib = DynamicLibrary.open(path);
+  final ping = lib
+      .lookup<NativeFunction<rust_ping>>('ping')
+      .asFunction<Ping>();
+
+  final pingAsync = lib
+      .lookup<NativeFunction<rust_ping_async>>('ping_async')
+      .asFunction<PingAsync>();
 
   print(Utf8.fromUtf8(ping(Utf8.toUtf8("FROM_DART").cast())));
+
+  pingAsync(
+      Utf8.toUtf8("FROM_DART_ASYNC").cast(),
+      Pointer.fromFunction<Callback>(callback)
+  );
 }
--- user/rust_c/src/lib.rs
+++ user/rust_c/src/lib.rs
@@ -1,19 +1,56 @@
+use std::cell::RefCell;
 use std::{ffi::{CStr, CString}, os::raw::c_char};
 
-fn move_in(s: *const c_char) -> String {
+use lazy_static::lazy_static;
+use tokio::sync::oneshot;
+
+fn str_in(s: *const c_char) -> String {
     let c_str = unsafe { CStr::from_ptr(s) };
     let s = c_str.to_str().unwrap();
     s.to_owned()
 }
 
-fn move_out(s: String) -> *mut c_char  {
+fn string_out(s: String) -> *mut c_char  {
     let c_str = CString::new(s).unwrap();
     c_str.into_raw()
 }
 
 #[no_mangle]
 pub extern "C" fn ping(message_raw: *const c_char) -> *mut c_char{
-    let message = move_in(message_raw);
+    let message = str_in(message_raw);
     let reply = format!("You sent '{}'. It works.", message);
-    move_out(reply)
+    string_out(reply)
+}
+
+thread_local!{
+    static RUNTIME: RefCell<Option<tokio::runtime::Runtime>> = RefCell::new(None);
+}
+
+lazy_static! {
+    static ref THREAD: std::thread::JoinHandle<()> =
+        std::thread::Builder::new().name("Morpheus Rust thread with Tokio".to_owned()).spawn( || {
+            RUNTIME.with( |rt_opt_cell| {
+                let mut rt_opt = rt_opt_cell.borrow_mut();
+                if let None = *rt_opt {
+                    let rt = tokio::runtime::Builder::new().enable_all().basic_scheduler().build().unwrap();
+                    rt_opt.replace(rt);
+                }
+                let (_sender, receiver) = oneshot::channel::<()>();
+                rt_opt.as_mut().unwrap().block_on(receiver).unwrap();
+            });
+        }).unwrap();
+
+}
+
+type PingCallback = extern "C" fn(*mut c_char) -> ();
+
+#[no_mangle]
+pub extern "C" fn ping_async(message_raw: *const c_char, callback: PingCallback) -> () {
+    lazy_static::initialize(&THREAD);
+
+    let message = str_in(message_raw);
+    let reply = format!("You sent '{}'. It works.", message);
+    tokio::runtime::Handle::current().spawn( async move {
+        callback( string_out(reply) )
+    });
 }
