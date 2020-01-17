a6d3921ac8131d3db51356e304ccf1d0d604fc79
Rache Bartmoss <bartmoss@tutanota.com>
2020-01-17 10:17:50 +0100

    Slightly redesigned version of Tokio Runtime for testing Dart-Rust interop

--- user/rust_c/src/lib.rs
+++ user/rust_c/src/lib.rs
@@ -1,5 +1,5 @@
-use std::cell::RefCell;
 use std::{ffi::{CStr, CString}, os::raw::c_char};
+use std::sync::{Arc, Mutex};
 
 use lazy_static::lazy_static;
 use tokio::sync::oneshot;
@@ -23,23 +23,22 @@ pub extern "C" fn ping(message_raw: *const c_char) -> *mut c_char{
 }
 
 thread_local!{
-    static RUNTIME: RefCell<Option<tokio::runtime::Runtime>> = RefCell::new(None);
+    static HANDLE: tokio::runtime::Handle = RUNTIME.lock().unwrap().handle().to_owned();
 }
 
 lazy_static! {
-    static ref THREAD: std::thread::JoinHandle<()> =
-        std::thread::Builder::new().name("Morpheus Rust thread with Tokio".to_owned()).spawn( || {
-            RUNTIME.with( |rt_opt_cell| {
-                let mut rt_opt = rt_opt_cell.borrow_mut();
-                if let None = *rt_opt {
-                    let rt = tokio::runtime::Builder::new().enable_all().basic_scheduler().build().unwrap();
-                    rt_opt.replace(rt);
-                }
-                let (_sender, receiver) = oneshot::channel::<()>();
-                rt_opt.as_mut().unwrap().block_on(receiver).unwrap();
-            });
-        }).unwrap();
+    static ref RUNTIME: Arc<Mutex<tokio::runtime::Runtime>> = Arc::new(Mutex::new(
+        tokio::runtime::Runtime::new()
+            .expect("Failed to initialize Tokio runtime")
+    ));
 
+    static ref THREAD: std::thread::JoinHandle<()> = {
+        let thread_name = "Morpheus Rust thread running the Tokio event loop".to_owned();
+        std::thread::Builder::new().name(thread_name).spawn( move || {
+            let (_sender, receiver) = oneshot::channel::<()>();
+            RUNTIME.lock().unwrap().block_on(receiver).unwrap();
+        }).unwrap()
+    };
 }
 
 type PingCallback = extern "C" fn(*mut c_char) -> ();
@@ -50,7 +49,9 @@ pub extern "C" fn ping_async(message_raw: *const c_char, callback: PingCallback)
 
     let message = str_in(message_raw);
     let reply = format!("You sent '{}'. It works.", message);
-    tokio::runtime::Handle::current().spawn( async move {
-        callback( string_out(reply) )
-    });
+    HANDLE.with(|handle|
+        handle.spawn( async move {
+            callback( string_out(reply) )
+        })
+    );
 }
